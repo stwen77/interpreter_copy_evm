a7856edc374ad4d1f7e12c66aaab0a8ee9b90c87
Shitong Wen <shitwen@qti.qualcomm.com>
2019-07-16 11:07:20 +0800

    add step stub

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,1 +1,44 @@
-extern crate num_bigint;
+use num_bigint::BigUint;
+use ethereum_types::{U256, H256, Address};
+
+/// Stepping result returned by interpreter.
+pub enum InterpreterResult {
+	/// The VM has already stopped.
+	Stopped,
+	/// The VM has just finished execution in the current step.
+	Done,
+	/// The VM can continue to run.
+	Continue,
+	Trap,
+}
+
+fn to_biguint(x: U256) -> BigUint {
+	let mut bytes = [0u8; 32];
+	x.to_little_endian(&mut bytes);
+	BigUint::from_bytes_le(&bytes)
+}
+
+fn from_biguint(x: BigUint) -> U256 {
+	let bytes = x.to_bytes_le();
+	U256::from_little_endian(&bytes)
+}
+
+pub struct Interpreter{
+
+}
+
+impl Interpreter{
+    fn exec(mut self: Box<Self>,ext: &mut Ext){
+        loop{
+            let result = self.step(ext);
+        }
+    }
+}
+
+impl Interpreter{
+    pub fn step(&mut self, ext: &mut Ext)->InterpreterResult{
+        InterpreterResult::Done
+    }
+}
+
+pub trait Ext {}

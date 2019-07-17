019b98a43d3af4641cfc522aa79bb146577ab193
Shitong Wen <shitwen@qti.qualcomm.com>
2019-07-17 14:11:40 +0800

    add code reader

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,10 +1,11 @@
+use std::marker::PhantomData;
+use std::sync::Arc;
+use std::{cmp, mem};
 use bit_set::BitSet;
 use bytes::Bytes;
 use ethereum_types::{Address, H256, U256};
 use num_bigint::BigUint;
-use std::marker::PhantomData;
-use std::sync::Arc;
-use std::{cmp, mem};
+
 
 /// Stepping result returned by interpreter.
 pub enum InterpreterResult {
@@ -45,6 +46,51 @@ struct InterpreterParams {
     //pub params_type: ParamsType,
 }
 
+enum InstructionResult {
+	Ok,
+	//UnusedGas(Gas),
+	JumpToPosition(U256),
+	StopExecutionNeedsReturn {
+		// Gas left.
+		//gas: Gas,
+		/// Return data offset.
+		init_off: U256,
+		/// Return data size.
+		init_size: U256,
+		/// Apply or revert state changes.
+		apply: bool,
+	},
+	StopExecution,
+	Trap,//(TrapKind),
+}
+
+struct CodeReader {
+	position: usize, //ProgramCounter,
+	code: Arc<Bytes>,
+}
+
+impl CodeReader {
+	/// Create new code reader - starting at position 0.
+	fn new(code: Arc<Bytes>) -> Self {
+		CodeReader {
+			code,
+			position: 0,
+		}
+	}
+
+	/// Get `no_of_bytes` from code and convert to U256. Move PC
+	fn read(&mut self, no_of_bytes: usize) -> U256 {
+		let pos = self.position;
+		self.position += no_of_bytes;
+		let max = cmp::min(pos + no_of_bytes, self.code.len());
+		U256::from(&self.code[pos..max])
+	}
+
+	fn len(&self) -> usize {
+		self.code.len()
+	}
+}
+
 fn to_biguint(x: U256) -> BigUint {
     let mut bytes = [0u8; 32];
     x.to_little_endian(&mut bytes);
@@ -69,7 +115,7 @@ pub struct Interpreter {
     //gasometer: Option<Gasometer<Cost>>,
     //stack: VecStack<U256>,
     resume_output_range: Option<(U256, U256)>,
-    //resume_result: Option<InstructionResult>,
+    resume_result: Option<InstructionResult>,
     last_stack_ret_len: usize,
     //_type: PhantomData<Cost>,
 }
@@ -87,10 +133,18 @@ impl Interpreter {
         if self.done {
             return InterpreterResult::Stopped;
         }
-        //self.step_inner(ext);
+        self.step_inner(ext);
         InterpreterResult::Done
     }
     fn step_inner(&mut self, ext: &mut Ext) -> Result<Never, InterpreterResult> {
+        let result = match self.resume_result.take(){
+            Some(result) => result,
+            None => {
+
+
+                InstructionResult::Ok
+            },
+        };
         Err(InterpreterResult::Continue)
     }
 }
